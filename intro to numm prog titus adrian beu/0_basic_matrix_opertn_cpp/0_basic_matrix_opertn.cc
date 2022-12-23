a28fef35e034adec0121bae9b20df41417c182c2
raziel66612 <aniketvishwakarma@gmail.com>
2022-12-23 18:13:06 -0500

    updated @ new folder

--- intro to numm prog titus adrian beu/0_basic_matrix_opertn_cpp/0_basic_matrix_opertn.cc
+++ intro to numm prog titus adrian beu/0_basic_matrix_opertn_cpp/0_basic_matrix_opertn.cc
@@ -2,6 +2,7 @@
 
 int main()
 {
-std::cout<< " Hello _world,testing_git from cpp";
+std::cout<< " Hello _world,testing_git from cpp"<< std :: endl;
+std::cout<< " Hello _world,testing_git from cpp again";
     return 0;
 }
