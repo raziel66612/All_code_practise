65b2d2c0d3bd6eb6e56b9ad26f9517dd08cba8e3
raziel66612 <aniketvishwakarma@gmail.com>
2022-12-24 03:14:55 -0500

    matrix created

--- Numerical_programming_Learning/1_basic_matrix_opertn_cpp/1_basic_matrix_opertn.cc
+++ Numerical_programming_Learning/1_basic_matrix_opertn_cpp/1_basic_matrix_opertn.cc
@@ -1,43 +1,65 @@
 #include <iostream>
 using namespace std;
 
-double* Vector(int x_min,int x_max)
-//******************************************
-//allocating memory for creating vector, using malloc  
-//******************************************
+double** Matrix(int x_min,int x_max,int y_min, int y_max)
+//*******************************************************
+//allocating memory for creating matrix, using malloc  
+//*******************************************************
 {
-double* p;
+int i;
+int Nx = x_max - x_min ;
+int Ny = y_max - y_min ; 
+double** p;
 
-p = (double*) malloc((size_t) ((x_max - x_min + 1)*sizeof(double)));
+p = (double**) malloc((size_t) (Nx*sizeof(double*)));
 if (!p){
-    printf("cannot allocaete vector \n");
+    printf("Cannot allocate : 1st Dimension \n");
     exit(1);
 }
-return p-x_min;     // adjust for offset // by using shift of origin.
+p-=x_min;     // adjust for offset // by using shift of origin.
+
+p[x_min] = (double*) malloc((size_t) (Nx*Ny*sizeof(double)));
+if (!p){
+    printf("Cannot allocate : 2nd Dimension \n");
+    exit(2);
+} else {
+    cout<< "Matrix created " << endl;
 }
 
-void DestroyVector(double* p,int x_min)
-//******************************************
+p[x_min]-=y_min;
+for (i=x_min+1 ; i<=x_max ; i++)  p[i]=p[i-1] + Ny;
+return p;
+}
+
+
+void DestroyMatrix(double** p,int x_min, int y_min)
+//*******************************************************
 //Destroying the allocated memory to prevent memory leak  
-//******************************************
+//*******************************************************
 {
-    free((void*) (p + x_min));      //re-adjusting/compensating for offset
+    free((void*) (p[x_min] + y_min));
+    free((void*) (p + x_min));      //
+    cout << "Matrix Destroyed"<< endl ;
 }
 
-
+// --------------------------- MAIN ---------------------------
 int main()
 {
 int xmin,xmax,ymin,ymax;
-xmin =-1 ; xmax = +1;
-ymin =-1 ; ymax = +1;
+xmin =-2 ; xmax = +2;
+ymin =-2 ; ymax = +2;
 
-double* a = Vector(xmin,xmax) ; // testing created vector
-        for(int i=xmin; i<=xmax; i++) a[i]=i;
+double** A = Matrix(xmin,xmax,ymin,ymax);
+for (int i= xmin ; i<=xmax ; i++){
+    for (int j= ymin ; j<=ymax ; j++)
+    { 
+        A[i][j] = i*j ; 
+        cout << A[i][j] << "   " ;
+    }
+cout<< endl ;
+}
 
-        for(int i=0;i<=xmax;i++){
-            cout << " array index: " << i << ";   array value : " << a[i] << endl;  
-        }
-
-cout<< endl <<"testing \n1\n2\n3" <<endl;
+DestroyMatrix(A,xmin,ymin);
+// cout<< endl <<"testing \n1\n2\n3" <<endl;
     return 0;
 }
