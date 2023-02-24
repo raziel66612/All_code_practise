a9251e01eff9226c12fcd1215f555cebbab22550
raziel66612 <aniketvishwakarma@gmail.com>
2023-02-23 19:07:06 -0500

    pythoon

--- Numerical_programming_Learning/1_basic_matrix_oprtn_python/1_basic_mat_oprtn.py
+++ Numerical_programming_Learning/1_basic_matrix_oprtn_python/1_basic_mat_oprtn.py
@@ -1,1 +1,11 @@
-print("hello world, git to pythoon")
+from math import *
+
+def MatPrint(a,n,m):
+    for i in range(1,n+1):
+        for j in range (1,m+1): print('{0:11.2e}'.format(a[i][j]), end="")
+        print()
+
+a=[[1, 2],[2, 1]]
+
+MatPrint(a)
+
