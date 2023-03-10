cc07de89903352ee121454ef14337f45142a4472
raziel66612 <aniketvishwakarma@gmail.com>
2023-03-09 22:52:57 -0500

    upd

---
+++ Numerical_programming_Learning/2_scientific_graphing_python/1_canvas.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()  
+root.title("Canvas")
+
+a=Canvas(root, width=600,height=450, bg = "white")
+a.pack()
+
+root.mainloop()  
---
+++ Numerical_programming_Learning/2_scientific_graphing_python/2_canvas_clock_static.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()  
+root.title("Canvas")
+
+Nx=300
+Ny=300   #canvas size
+a=Canvas(root, width=Nx,height=Ny, bg = "white")
+a.pack()
+
+a.create_line(150,150,100,100,fill="black",arrow=LAST, width=3)
+a.create_line(150,150,150,250,fill="red",arrow=LAST)
+a.create_rectangle(50, 50, 250, 250, width=2)
+a.create_oval(50,50,250,250)
+a.create_text(150,75,text = "10:30")
+root.mainloop()  
